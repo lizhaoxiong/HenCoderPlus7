d77cc021f5a3cc5b1b7de62cbbc04bf86403b906
lizhaoxiong002 <lizhaoxiong002@ke.com>
2021-01-06 18:23:55 +0800

    [约束布局]新增:统一旋转缩放、位移、设置Layer

--- ConstraintLayout/constraintlayout/src/main/java/com/hencoder/Helpers.kt
+++ ConstraintLayout/constraintlayout/src/main/java/com/hencoder/Helpers.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import androidx.constraintlayout.helper.widget.Layer
 import androidx.constraintlayout.widget.Group
 
 import com.hencoder.constraintlayout.R
@@ -13,18 +14,18 @@ class Helpers : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_group)
-        findViewById<Button>(R.id.button).setOnClickListener {
-            findViewById<Group>(R.id.group).visibility = View.GONE
-        }
-
-
-//        setContentView(R.layout.activity_layer)
+//        setContentView(R.layout.activity_group)
 //        findViewById<Button>(R.id.button).setOnClickListener {
-//            findViewById<Layer>(R.id.layer).rotation = 45f
-//            findViewById<Layer>(R.id.layer).translationY = 100f
-//            findViewById<Layer>(R.id.layer).translationX = 100f
+//            findViewById<Group>(R.id.group).visibility = View.GONE
 //        }
+
+
+        setContentView(R.layout.activity_layer)
+        findViewById<Button>(R.id.button).setOnClickListener {
+            findViewById<Layer>(R.id.layer).rotation = 45f
+            findViewById<Layer>(R.id.layer).translationY = 100f
+            findViewById<Layer>(R.id.layer).translationX = 100f
+        }
 //
 //        setContentView(R.layout.activity_barrier)
 
